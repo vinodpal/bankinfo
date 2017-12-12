50acb7790c60f0399e3dc3665dabe5502d0d9fc6
Vinod Kumar Pal <vinodpal458@gmail.com>
2017-12-12 13:46:36 +0530

    Update Branch.java

--- src/main/java/com/fyle_backend_test/bankInfo/bank/Branch.java
+++ src/main/java/com/fyle_backend_test/bankInfo/bank/Branch.java
@@ -6,6 +6,6 @@ import com.fyle_backend_test.bankInfo.model.Bank;
 
 
 public interface Branch {
-	public List<Bank> getAllBankBranchDetails(String name, String city);
+	public List<Bank> getAllBankBranchDetails(String name , String city);
 	public Bank getBankBranchDetails(String ifsc);
 }
